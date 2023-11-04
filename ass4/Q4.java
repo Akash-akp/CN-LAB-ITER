7c0f170c6e72fef51385f06094e4d7d30ad2dbd5
Akash-akp <akashakp0037@gmail.com>
2023-11-04 07:05:09 +0000

    create Q4 of assignment 4

--- ass4/Q4.java
+++ ass4/Q4.java
@@ -1,6 +1,29 @@
-import java.net.*;
-public class Q4 {
-    public static void main(String[] args) throws{
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 
+public class Q4 {
+    public static void main(String[] args) {
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaces.nextElement();
+                System.out.println("Interface Name: " + networkInterface.getName());
+                System.out.println("Display Name: " + networkInterface.getDisplayName());
+
+                byte[] macAddress = networkInterface.getHardwareAddress();
+                if (macAddress != null) {
+                    System.out.print("MAC Address: ");
+                    for (int i = 0; i < macAddress.length; i++) {
+                        System.out.format("%02X%s", macAddress[i], (i < macAddress.length - 1) ? ":" : "");
+                    }
+                    System.out.println();
+                }
+
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
